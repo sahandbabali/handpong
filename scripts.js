b471516cb1e7c197fdafbc13ae420ccbde312eb6
sahandbabali <sahand.babali@gmail.com>
2021-07-13 01:28:03 +0430

    css changes

--- scripts.js
+++ scripts.js
@@ -6,67 +6,64 @@ let isVideo = false;
 let model = null;
 var posx = 0;
 
-
 const modelParams = {
-    flipHorizontal: true,   // flip e.g for video  
-    maxNumBoxes: 3,        // maximum number of boxes to detect
-    iouThreshold: 0.5,      // ioU threshold for non-max suppression
-    scoreThreshold: 0.6,    // confidence threshold for predictions.
-}
+  flipHorizontal: true, // flip e.g for video
+  maxNumBoxes: 3, // maximum number of boxes to detect
+  iouThreshold: 0.5, // ioU threshold for non-max suppression
+  scoreThreshold: 0.6, // confidence threshold for predictions.
+};
 
 function startVideo() {
-    handTrack.startVideo(video).then(function (status) {
-        console.log("video started", status);
-        if (status) {
-            isVideo = true
-            runDetection()
-        } else {
-        }
-    });
+  handTrack.startVideo(video).then(function (status) {
+    console.log("video started", status);
+    if (status) {
+      isVideo = true;
+      runDetection();
+    } else {
+    }
+  });
 }
 
 function runDetection() {
-    model.detect(video).then(predictions => {
-      //   console.log("Predictions: ", predictions);
-        model.renderPredictions(predictions, canvas, context, video);
+  model.detect(video).then((predictions) => {
+    //   console.log("Predictions: ", predictions);
+    model.renderPredictions(predictions, canvas, context, video);
 
-        for (const obj of predictions) {
-              if(obj.label !== "face") {
-                    console.log(obj.bbox)
+    for (const obj of predictions) {
+      if (obj.label !== "face") {
+        console.log(obj.bbox);
 
-                    posx = obj.bbox[0];
-              }
-            
-          }
+        posx = obj.bbox[0];
+      }
+    }
 
-        if (isVideo) {
-            requestAnimationFrame(runDetection);
-        }
-    });
+    if (isVideo) {
+      requestAnimationFrame(runDetection);
+    }
+  });
 }
 
 // Load the model.
-handTrack.load(modelParams).then(lmodel => {
-    model = lmodel
-    startVideo();
+handTrack.load(modelParams).then((lmodel) => {
+  model = lmodel;
+  startVideo();
 });
-
 
 var xBall = Math.floor(Math.random() * 300) + 50;
 var yBall = 50;
 var xSpeed = (2, 7);
 var ySpeed = (-7, -2);
-var score = 0
+var score = 0;
 
 function setup() {
-  createCanvas(450, 400);
+  var myCanvas = createCanvas(450, 400);
+  myCanvas.parent("cont");
 }
 
 function draw() {
+  background("#457b9d");
 
-  background(0);
-
-  fill('#ffffff');
+  fill("#ffffff");
   rect(posx, 375, 90, 15);
 
   move();
@@ -74,9 +71,9 @@ function draw() {
   bounce();
   paddle();
 
-  fill('#d9c3f7');
+  fill("#ffffff");
   textSize(24);
-  text("Score: " + score, 10, 25);
+  text("Score: " + score, 200, 25);
 }
 function move() {
   xBall += xSpeed;
@@ -84,27 +81,22 @@ function move() {
 }
 
 function bounce() {
-  if (xBall < 10 ||
-    xBall > 400 - 10) {
+  if (xBall < 10 || xBall > 400 - 10) {
     xSpeed *= -1;
   }
-  if (yBall < 10 ||
-    yBall > 400 - 10) {
+  if (yBall < 10 || yBall > 400 - 10) {
     ySpeed *= -1;
   }
 }
 
 function display() {
-  fill('#d9c3f7');
+  fill("#ffffff");
   ellipse(xBall, yBall, 20, 20);
 }
 function paddle() {
-  if ((xBall > posx &&
-      xBall < posx + 90) &&
-    (yBall + 10 >= 375)) {
+  if (xBall > posx && xBall < posx + 90 && yBall + 10 >= 375) {
     xSpeed *= -1;
     ySpeed *= -1;
     score++;
-
   }
 }
